18e07f0806fa6bf4ec46d70850d8c8d37ee102cf
fadhfaiz <faadh.faaiz@gmail.com>
2019-06-25 22:45:49 +0700

    test satu dua tiga

---
+++ application/views/homepage/index.php
@@ -0,0 +1,19 @@
+<section class="container">
+	<div class="row my-4">
+		<div class="col-6">
+			<form>
+				<div class="form-group">
+					<label for="inputgram" class="h5">Masukan Kalimat</label>
+					<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+						else.</small> -->
+				</div>
+				<button type="submit" class="btn btn-primary">Proses</button>
+			</form>
+		</div>
+
+		<div class="col-6">
+            <p class="h5">Hasil Bigrams</p>
+		</div>
+	</div>
+</section>
---
+++ application/views/templates/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet"> 
+	<title>Hello, world!</title>
+</head>
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container">
+			<a class="navbar-brand" href="#">N-Grams NLP</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<!-- <li class="nav-item">
+						<a class="nav-link" href="#">Pricing</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+					</li> -->
+				</ul>
+			</div>
+		</div>
+	</nav>
