d9248ddbdd3b102495ddfe3147ee9a019beeb210
insanz01 <insankamil002@gmail.com>
2019-07-02 09:14:43 +0700

    menambahkan algoritma untuk melakukan ngrams

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -1,11 +1,32 @@
 <?php
-    class Homepage extends CI_Controller
+class Homepage extends CI_Controller
+{
+    public function __construct()
     {
-        public function index()
-        {
-            $this->load->view('templates/header');
-            $this->load->view('homepage/index');
-            $this->load->view('templates/footer');
-        }
+        parent::__construct();
+        $this->load->library('form_validation');
     }
-?>
+
+    public function index()
+    {
+        $hasil = [];
+        $this->form_validation->set_rules('kalimat', 'Kalimat', 'required');
+
+        if ($this->form_validation->run() != FALSE) {
+            $kalimat = $this->input->post('kalimat');
+
+            $oko_dan_faiz = explode(" ", $kalimat);
+
+            for ($i = 0; $i < count($oko_dan_faiz) - 1; $i++) {
+                $hasil[] = $oko_dan_faiz[$i] . " " . $oko_dan_faiz[$i + 1];
+            }
+
+            $data['hasil'] = $hasil;
+        }
+
+
+        $this->load->view('templates/header');
+        $this->load->view('homepage/index', $data);
+        $this->load->view('templates/footer');
+    }
+}
--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -1,19 +1,32 @@
 <section class="container">
 	<div class="row my-4">
 		<div class="col-6">
-			<form>
+			<form action="<?= base_url() ?>" method="post">
 				<div class="form-group">
 					<label for="inputgram" class="h5">Masukan Kalimat</label>
-					<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-						else.</small> -->
+					<input type="text" class="form-control" name="kalimat" id="inputgram">
 				</div>
 				<button type="submit" class="btn btn-primary">Proses</button>
 			</form>
 		</div>
 
 		<div class="col-6">
-            <p class="h5">Hasil Bigrams</p>
+			<p class="h5">Hasil Bigrams</p>
+			<p>
+				<?php
+				if ($hasil) :
+					$i = 1;
+					?>
+					<?php foreach ($hasil as $oko) : ?>
+						<?= $oko; ?>
+						<?php if (count($hasil) == $i++) : ?>
+							<?= '.' ?>
+						<?php else : ?>
+							<?= ',' ?>
+						<?php endif; ?>
+					<?php endforeach; ?>
+				<?php endif; ?>
+			</p>
 		</div>
 	</div>
 </section>
